16004c090dadb65f7ec7b4a643b13b86a9f3a7ea
Ray Bergholm <raymond.bergholm@sanoma.com>
2018-02-07 11:12:20 +0200

    convert tabs to spaces

--- src/v2/LocalFileReader.v2.js
+++ src/v2/LocalFileReader.v2.js
@@ -2,67 +2,67 @@
 // Copyright 2018 Raymond Bergholm - https://github.com/raybergholm - MIT licence.
 
 const LocalFileReader = Object.freeze({
-	ReadModes: Object.freeze({
-		ArrayBuffer: "A",
-		Binary: "B",
-		DataURL: "D",
-		Text: "T"
-	}),
-	read: (inputFiles, inputReadMode = "T") => {
-		const errorTexts = {
-			UNKNOWN_ENVIRONMENT: "No Window object, what environment are you running on?",
-			FILE_API_UNSUPPORTED: "File APIs unsupported: File, FileReader, FileList or Blob is missing",
-			NO_FILELIST: "No file list given",
-			INVALID_FILELIST: "File list input was invalid (not a FileList object)",
-			UNEXPECTED_READ_MODE: "Unexpected read mode: check if the input value matches the ReadMode enumeration",
-		};
+    ReadModes: Object.freeze({
+        ArrayBuffer: "A",
+        Binary: "B",
+        DataURL: "D",
+        Text: "T"
+    }),
+    read: (inputFiles, inputReadMode = "T") => {
+        const errorTexts = {
+            UNKNOWN_ENVIRONMENT: "No Window object, what environment are you running on?",
+            FILE_API_UNSUPPORTED: "File APIs unsupported: File, FileReader, FileList or Blob is missing",
+            NO_FILELIST: "No file list given",
+            INVALID_FILELIST: "File list input was invalid (not a FileList object)",
+            UNEXPECTED_READ_MODE: "Unexpected read mode: check if the input value matches the ReadMode enumeration",
+        };
 
-		// Sanity checks
-		if (!window) {
-			throw new Error(errorTexts.UNKNOWN_ENVIRONMENT);
-		} else if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-			throw new Error(errorTexts.FILE_API_UNSUPPORTED);
-		} else if (!inputFiles) {
-			throw new Error(errorTexts.NO_FILELIST);
-		} else if (!inputFiles instanceof FileList) {
-			throw new Error(errorTexts.INVALID_FILELIST);
-		} else if (Object.values(LocalFileReader.ReadModes).indexOf(inputReadMode) === -1) {
-			throw new Error(errorTexts.UNEXPECTED_READ_MODE);
-		}
+        // Sanity checks
+        if (!window) {
+            throw new Error(errorTexts.UNKNOWN_ENVIRONMENT);
+        } else if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+            throw new Error(errorTexts.FILE_API_UNSUPPORTED);
+        } else if (!inputFiles) {
+            throw new Error(errorTexts.NO_FILELIST);
+        } else if (!inputFiles instanceof FileList) {
+            throw new Error(errorTexts.INVALID_FILELIST);
+        } else if (Object.values(LocalFileReader.ReadModes).indexOf(inputReadMode) === -1) {
+            throw new Error(errorTexts.UNEXPECTED_READ_MODE);
+        }
 
-		return Promise.all(Array.from(inputFiles).map((file) => {
-			return new Promise((resolve, reject) => {
-				let fileReader = new FileReader();
+        return Promise.all(Array.from(inputFiles).map((file) => {
+            return new Promise((resolve, reject) => {
+                let fileReader = new FileReader();
 
-				fileReader.addEventListener("load", (evt) => {
-					resolve({
-						file: file,
-						content: evt.target.result
-					});
-				});
+                fileReader.addEventListener("load", (evt) => {
+                    resolve({
+                        file: file,
+                        content: evt.target.result
+                    });
+                });
 
-				fileReader.addEventListener("error", (evt) => {
-					reject({
-						file: file,
-						error: fileReader.error
-					});
-				});
+                fileReader.addEventListener("error", (evt) => {
+                    reject({
+                        file: file,
+                        error: fileReader.error
+                    });
+                });
 
-				switch (inputReadMode) {
-					case LocalFileReader.ReadModes.ArrayBuffer:
-						fileReader.readAsArrayBuffer(file);
-						break;
-					case LocalFileReader.ReadModes.Binary:
-						fileReader.readAsBinaryString(file);
-						break;
-					case LocalFileReader.ReadModes.DataURL:
-						fileReader.readAsDataURL(file);
-						break;
-					case LocalFileReader.ReadModes.Text:
-						fileReader.readAsText(file);
-						break;
-				}
-			});
-		}));
-	}
+                switch (inputReadMode) {
+                    case LocalFileReader.ReadModes.ArrayBuffer:
+                        fileReader.readAsArrayBuffer(file);
+                        break;
+                    case LocalFileReader.ReadModes.Binary:
+                        fileReader.readAsBinaryString(file);
+                        break;
+                    case LocalFileReader.ReadModes.DataURL:
+                        fileReader.readAsDataURL(file);
+                        break;
+                    case LocalFileReader.ReadModes.Text:
+                        fileReader.readAsText(file);
+                        break;
+                }
+            });
+        }));
+    }
 });
--- src/v3/LocalFileReader.v3.js
+++ src/v3/LocalFileReader.v3.js
@@ -2,66 +2,66 @@
 // Copyright 2018 Raymond Bergholm - https://github.com/raybergholm - MIT licence.
 
 export const ReadModes = Object.freeze({
-	ArrayBuffer: "A",
-	Binary: "B",
-	DataURL: "D",
-	Text: "T"
+    ArrayBuffer: "A",
+    Binary: "B",
+    DataURL: "D",
+    Text: "T"
 });
 
 export const LocalFileReader = (inputFiles, inputReadMode = "T") => {
-	const errorTexts = {
-		UNKNOWN_ENVIRONMENT: "No Window object, what environment are you running on?",
-		FILE_API_UNSUPPORTED: "File APIs unsupported: File, FileReader, FileList or Blob is missing",
-		NO_FILELIST: "No file list given",
-		INVALID_FILELIST: "File list input was invalid  (not a FileList object)",
-		UNEXPECTED_READ_MODE: "Unexpected read mode: check if the input value matches the ReadMode enumeration",
-	};
+    const errorTexts = {
+        UNKNOWN_ENVIRONMENT: "No Window object, what environment are you running on?",
+        FILE_API_UNSUPPORTED: "File APIs unsupported: File, FileReader, FileList or Blob is missing",
+        NO_FILELIST: "No file list given",
+        INVALID_FILELIST: "File list input was invalid  (not a FileList object)",
+        UNEXPECTED_READ_MODE: "Unexpected read mode: check if the input value matches the ReadMode enumeration",
+    };
 
-	// Sanity checks
-	if (!window) {
-		throw new Error(errorTexts.UNKNOWN_ENVIRONMENT);
-	} else if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
-		throw new Error(errorTexts.FILE_API_UNSUPPORTED);
-	} else if (!inputFiles) {
-		throw new Error(errorTexts.NO_FILELIST);
-	} else if (!inputFiles instanceof FileList) {
-		throw new Error(errorTexts.INVALID_FILELIST);
-	} else if (Object.values(ReadModes).indexOf(inputReadMode) === -1) {
-		throw new Error(errorTexts.UNEXPECTED_READ_MODE);
-	}
+    // Sanity checks
+    if (!window) {
+        throw new Error(errorTexts.UNKNOWN_ENVIRONMENT);
+    } else if (!window.File || !window.FileReader || !window.FileList || !window.Blob) {
+        throw new Error(errorTexts.FILE_API_UNSUPPORTED);
+    } else if (!inputFiles) {
+        throw new Error(errorTexts.NO_FILELIST);
+    } else if (!inputFiles instanceof FileList) {
+        throw new Error(errorTexts.INVALID_FILELIST);
+    } else if (Object.values(ReadModes).indexOf(inputReadMode) === -1) {
+        throw new Error(errorTexts.UNEXPECTED_READ_MODE);
+    }
 
-	return Promise.all(Array.from(inputFiles).map((file) => {
-		return new Promise((resolve, reject) => {
-			let fileReader = new FileReader();
+    return Promise.all(Array.from(inputFiles).map((file) => {
+        return new Promise((resolve, reject) => {
+            let fileReader = new FileReader();
 
-			fileReader.addEventListener("load", (evt) => {
-				resolve({
-					file: file,
-					content: evt.target.result
-				});
-			});
+            fileReader.addEventListener("load", (evt) => {
+                resolve({
+                    file: file,
+                    content: evt.target.result
+                });
+            });
 
-			fileReader.addEventListener("error", (evt) => {
-				reject({
-					file: file,
-					error: fileReader.error
-				});
-			});
+            fileReader.addEventListener("error", (evt) => {
+                reject({
+                    file: file,
+                    error: fileReader.error
+                });
+            });
 
-			switch (inputReadMode) {
-				case ReadModes.ArrayBuffer:
-					fileReader.readAsArrayBuffer(file);
-					break;
-				case ReadModes.Binary:
-					fileReader.readAsBinaryString(file);
-					break;
-				case ReadModes.DataURL:
-					fileReader.readAsDataURL(file);
-					break;
-				case ReadModes.Text:
-					fileReader.readAsText(file);
-					break;
-			}
-		});
-	}));
+            switch (inputReadMode) {
+                case ReadModes.ArrayBuffer:
+                    fileReader.readAsArrayBuffer(file);
+                    break;
+                case ReadModes.Binary:
+                    fileReader.readAsBinaryString(file);
+                    break;
+                case ReadModes.DataURL:
+                    fileReader.readAsDataURL(file);
+                    break;
+                case ReadModes.Text:
+                    fileReader.readAsText(file);
+                    break;
+            }
+        });
+    }));
 }
